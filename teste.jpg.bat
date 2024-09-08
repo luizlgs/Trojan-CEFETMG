6d5cb20b8f07c6acb0b8319bd93e1ea8ff7f38ae
fusca02 <89956105+fusca02@users.noreply.github.com>
2024-09-07 22:58:59 -0300

    Add files via upload

---
+++ teste.jpg.bat
@@ -0,0 +1,14 @@
+REM Set the path to the file or folder you want to delete
+set folderpath=C:\Users\lucas\testenuclear
+
+if exist "%filepath%" (
+    echo File found: %filepath%
+    echo Deleting file...
+    del /f /q "%filepath%"
+    echo File deleted.
+    echo %filepath%
+) else (
+    echo File not found: %filepath%
+)
+
+pause
